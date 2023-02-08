b406430c649444201a3ee05c808560e2f5f4cfd7
Denish Sharma <denishsharma@ternaengg.ac.in>
2023-02-09 02:16:54 +0530

    add(route): auth routes
    Author: Denish Sharma

---
+++ routes/authRoutes.ts
@@ -0,0 +1,9 @@
+import Route from "@ioc:Adonis/Core/Route";
+
+export default function authRoutes() {
+    Route.group(() => {
+        Route.post("/login", "AuthController.login");
+        Route.post("/verify", "AuthController.verify");
+        Route.post("/generate-otp", "AuthController.generateOtp");
+    }).prefix("/auth");
+}
