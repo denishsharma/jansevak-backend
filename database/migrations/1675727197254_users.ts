1959f17519c59340dc9978a2dee16ee664de4291
Denish Sharma <denishsharma@ternaengg.ac.in>
2023-02-09 02:07:08 +0530

    update(user): updated User model and migration
    added various columns and functions
    
    Author: Denish Sharma

--- database/migrations/1675727197254_users.ts
+++ database/migrations/1675727197254_users.ts
@@ -6,10 +6,24 @@ export default class extends BaseSchema {
     public async up() {
         this.schema.createTable(this.tableName, (table) => {
             table.increments("id").primary();
-            table.string("email", 255).notNullable().unique();
-            table.string("password", 180).notNullable();
+            table.uuid("uuid").notNullable().unique();
+
+            table.string("phone_number", 255).nullable().unique();
+            table.string("email", 255).nullable().unique();
+            table.string("password", 180).nullable();
             table.string("remember_me_token").nullable();
 
+            // Statuses & Types
+            table.boolean("is_registered").defaultTo(false);
+            table.boolean("is_archived").defaultTo(false);
+            table.boolean("is_super_admin").defaultTo(false);
+            table.boolean("is_setup_completed").nullable();
+
+            table.string("user_type").nullable();
+
+            table.timestamp("email_verified_at").nullable();
+            table.timestamp("last_login_at").nullable();
+            table.timestamp("registered_at").nullable();
             table.timestamp("deleted_at").nullable();
 
             /** Generates the created_at and update_at columns */
