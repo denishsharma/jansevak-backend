14525ebd5437ba696d8f539a3231f2e975dd9afb
Denish Sharma <denishsharma@ternaengg.ac.in>
2023-02-15 13:15:03 +0530

    update(profile): support for profile picture
    Author: Denish Sharma

--- app/Models/Profile.ts
+++ app/Models/Profile.ts
@@ -3,6 +3,8 @@ import { BaseModel, computed, column, belongsTo, BelongsTo, hasOne, HasOne } fro
 import User from "App/Models/User";
 import Address from "App/Models/Address";
 import { string } from "@ioc:Adonis/Core/Helpers";
+import { createHash } from "crypto";
+import Drive from "@ioc:Adonis/Core/Drive";
 
 export default class Profile extends BaseModel {
     @column({ isPrimary: true })
@@ -32,6 +34,9 @@ export default class Profile extends BaseModel {
     @column()
     public email: string | null;
 
+    @column()
+    public avatar: string | null;
+
     @column.dateTime()
     public birthDate: DateTime | null;
 
@@ -58,14 +63,38 @@ export default class Profile extends BaseModel {
     @hasOne(() => Address)
     public address: HasOne<typeof Address>;
 
-    @computed()
+    /**
+     * Get the full name.
+     */
+    @computed({ serializeAs: "full_name" })
     public get fullName(): string {
+        if (!this.firstName && !this.middleName && !this.lastName) return "";
         return string.condenseWhitespace(`${this.firstName} ${this.middleName} ${this.lastName}`);
     }
 
-    @computed()
+    /**
+     * Get the initials and last name.
+     */
+    @computed({ serializeAs: "initials_and_last_name" })
     public get initialsAndLastName(): string {
+        if (!this.firstName && !this.middleName && !this.lastName) return "";
         return string.condenseWhitespace(`${this.firstName?.charAt(0)} ${this.middleName?.charAt(0)} ${this.lastName}`);
+    }
+
+    /**
+     * Get url of the avatar.
+     */
+    @computed({ serializeAs: "avatar_url" })
+    public get avatarUrl(): string {
+        if (!this.avatar) {
+            if (!this.firstName && !this.lastName) return "https://api.dicebear.com/5.x/shapes/png?backgroundType=gradientLinear&size=128&seed=" + createHash("shake256", { outputLength: 8 }).update(this.id.toString()).digest("hex");
+
+            return "https://ui-avatars.com/api/?background=2563eb&color=fff&size=128&name=" + this.firstName?.charAt(0) + "+" + this.lastName?.charAt(0);
+        }
+
+        let url;
+        Drive.getSignedUrl(this.avatar).then(r => url = r);
+        return url;
     }
 
 }
